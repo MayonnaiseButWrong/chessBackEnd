751fcc38c307b129024149f54648fb518d23b816
MayonnaiseButWrong <manaschopade@gmail.com>
2023-02-01 02:14:24 +0000

    fixed xenonnumbers

--- translations.py
+++ translations.py
@@ -10,28 +10,29 @@ def isintager(x):
 def to_xenonnumber(gamelist):
 
     primeNumbers=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283.293,307,311,313]
-    Pieceslist=['00','WR','WN','WB','WQ','WK','WP','BR','BN','BB','BQ','BK','BP']
+    Pieceslist=['MT','WR','WN','WB','WQ','WK','WP','BR','BN','BB','BQ','BK','BP']
     xenonnumber=1
     n1=0                                                                                              #announcing variables
     n2=0
     n3=0
 
-    for i in range(0,64):                                                                               #there is garunteed to be 36 items in the game list inputted and we need to translate all of them
+    for j in range(0,8):                                                                               #there is garunteed to be 36 items in the game list inputted and we need to translate all of them
+        for i in range(0,8):
+            n1=int(primeNumbers[i*j])                                                                       #getting the actual numbers needed for the calculation from the list that they're in
+            n2=int(Pieceslist.index(gamelist[j][i]))
 
-        n1=int(primeNumbers[i])                                                                       #getting the actual numbers needed for the calculation from the list that they're in
-        n2=int(Pieceslist.index(gamelist[i]))
-
-        n3=n1**n2                                                                                     #the respective prime number to the power of the xenon number for the piece at the corresponding place, multiplied by all the others goves the unique xenon number for the frame
-        xenonnumber=xenonnumber*n3
-        print(i,n1,n2,n3,xenonnumber)
+            n3=n1**n2                                                                                     #the respective prime number to the power of the xenon number for the piece at the corresponding place, multiplied by all the others goves the unique xenon number for the frame
+            xenonnumber=xenonnumber*n3
+            print(i,n1,n2,n3,xenonnumber)
 
     return xenonnumber
 
 def to_gamelist(xenonnumber):
 
     primeNumbers=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283.293,307,311,313]
-    Pieceslist=['00','WR','WN','WB','WQ','WK','WP','BR','BN','BB','BQ','BK','BP']
-    gamelist=[]                                                                                         #announcing the variables
+    Pieceslist=['MT','WR','WN','WB','WQ','WK','WP','BR','BN','BB','BQ','BK','BP']
+    gamelist=[]                                                                                      #announcing the variables
+    direction=[]
     n2=0
     numcount=0
     Piece=''
@@ -47,8 +48,10 @@ def to_gamelist(xenonnumber):
             numcount+=1                                                                                 #the prime factorisation of the xenon number corresponds to the piece at the square corresponding to the prime number used. the prime numbers are tested from smalled to lagest resulting in the pieces from A1 to H8 on the chess board in order
             xenonnumber=xenonnumber//n2
         Piece=Pieceslist[numcount]                                                                       #translating the piece xenon number to the piece it corresponds to
-        gamelist.append(Piece)
-    
+        direction.append(Piece)
+        if i%8==0 and i!=0:
+            gamelist.append(direction)
+            direction=[]
     return gamelist
 
 def toCoOrdinates(inputTuple):
